ddb042f660a3c5e31a1fd04f5ceb65c0523359c6
Vita Kostiuk <vitagrebennik@gmail.com>
2022-06-27 14:45:55 +0300

    fixed bag

--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -1,18 +1,11 @@
 import ContactsPage from './ContactsPage';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { useSelector } from 'react-redux';
 import { authSelectors } from 'redux/auth';
 
 const NotFound = () => {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
-  return (
-    <>
-      {isLoggedIn ? <ContactsPage /> : <p>Such page is not exists</p>}
-      <ToastContainer />
-    </>
-  );
+  return <>{isLoggedIn ? <ContactsPage /> : <p>Such page is not exists</p>}</>;
 };
 
 export default NotFound;
